1fe8ba7087c17be1798de01e4a62330b09be9447
avrhahm <am500148148@gmail.com>
2026-09-02 15:52:29 +0300

    .

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-import express from'express'
+import express from 'express'
 import { movies } from './movies.js';
+import cors from'cors'
 
 const app = express();
+app.use(cors())
 const PORT = 3000;
 
 
 app.get('/api/data', (req, res) => {
+    console.log(movies.length);
+    
     res.json(movies);
 });
 
